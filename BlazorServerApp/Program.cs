312bf8152ef87893909fbffb33f6a423257f98d7
B <B@MYMAINPC>
2023-11-25 13:39:11 +0000

    cleanup

--- BlazorServerApp/Program.cs
+++ BlazorServerApp/Program.cs
@@ -2,16 +2,17 @@ using SensorMonitoring.BlazorServerApp.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SensorMonitoring.BlazorServerApp;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<SensorService>();
 builder.Services.Configure<ServerAppOptions>(
     builder.Configuration.GetSection(nameof(ServerAppOptions).ToString()));
+builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
